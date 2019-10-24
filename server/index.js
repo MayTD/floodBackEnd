4a80bb5a7758c7ebdde26c85f59fc1ae7bfd1098
May <may0000ff@gmail.com>
2019-10-24 14:22:40 -0500

    redirect login id

--- server/index.js
+++ server/index.js
@@ -112,7 +112,7 @@ app.get('/auth/google', passport.authenticate('google', {
 app.get('/auth/google/callback', passport.authenticate('google', {
   failureRedirect: '/',
 }), (req, res) => {
-  res.redirect(`/?id=${req.user.rows[0].googleid}`);
+  res.redirect(`/?id=${req.user.googleid}`);
 });
 
 // Logout route
